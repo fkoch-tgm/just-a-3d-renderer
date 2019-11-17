1d3966c259388b4146734517c177e1960981483d
fkoch-tgm <fkoch@student.tgm.ac.at>
2019-11-17 01:28:10 +0100

    updated Test

--- src/tests/Test_Point_and_Vector.java
+++ src/tests/Test_Point_and_Vector.java
@@ -15,12 +15,20 @@ public class Test_Point_and_Vector {
 		
 		v2 = p1.subtractPointFromPoint(p2);
 		
-		v1 = v1.addVectorToVector(v2);
+		v1.addVectorToVector(v2);
 		
 		p1.addVectorToPoint(v1);
 		p1.drawPoint();
 		
 		p2.subtractVectorFromPoint(v2);
 		p2.drawPoint();
+		
+		/*
+		 * Should output:
+		 * 	Point [tuple=[1, 2, 1]]
+		 * 	Point [tuple=[0, 4, 4]]
+		 * 	Point [tuple=[4, 0, -2]]
+		 * 	Point [tuple=[-1, 6, 7]]
+		 */
 	}
 }
